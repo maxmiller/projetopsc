c2fa31b5ffeb556aff77637fec6bbe58381528c2
Julio Melo <julio.engcomp@gmail.com>
2011-06-06 12:32:44 -0300

    mudando o tamanho da palavra para 16 bits pois o processador do livro usa esse tamanho

--- config.h
+++ config.h
@@ -1,7 +1,14 @@
 #ifndef config_h
 #define config_h
 
-#define WORD_SIZE 8
+#define WORD_SIZE 16
 #define MEMORY_SIZE 32
+
+typedef enum UlaOperation{
+	ulaOperation_add,
+	ulaOperation_and,
+	ulaOperation_multiply,
+	ulaOperation_doNothing,
+};
 
 #endif
