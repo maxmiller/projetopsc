4176d08c4883ebc79382e2ee613d1210595e25fb
Max Miller <aspmiller007@gmail.com>
2011-06-07 22:52:39 -0300

    teste memoria

--- memory.h
+++ memory.h
@@ -1,28 +1,22 @@
-#include "systemc.h"
-
 #ifndef RULA_h
 #define RULA_h
 
-SC_MODULE(memory) {
-public:
-	//portas
-	sc_in<bool> clk, rst, writer;
-	sc_in<sc_uint<8> > addres;
-	sc_in<sc_uint<8> > datai;
-	sc_out<sc_uint<8> > datao;
-	//internos
-	sc_uint<8> mem[32];
-	
-public:
-	void t_sync_mem();
-	
-	SC_CTOR(memory) {
-		SC_CTHREAD(t_sync_mem, clk.pos());
-		sensitive << rst;
-		sensitive << clk.pos();
-		//sensitive << writer;
-	//	watching(rst.delayed());
-	}
+SC_MODULE(Memory) {
+	public:
+		//portas
+		sc_in<bool> clk, rst, write;
+		sc_in<sc_uint<8> > address;
+		sc_in<sc_uint<8> > datai;
+		sc_out<sc_uint<8> > datao;
+		//internos
+		sc_uint<sc_uint<8> > mem[32];
+	public:
+		void t_sync_mem();
+
+		SC_CTOR(sync_mem) {
+			SC_CTHREAD(t_sync_mem, clk.pos());
+			watching(rst.delayed());
+		}
 };
 
 #endif
