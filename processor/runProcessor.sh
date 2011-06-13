d96bd64c7a94482a3d20486628855fe3dc0337b1
Julio Melo <julio.engcomp@gmail.com>
2011-06-12 23:39:48 -0300

    adicionando instruções de configuração do script

--- processor/runProcessor.sh
+++ processor/runProcessor.sh
@@ -1,2 +1,4 @@
+#o primeiro path corresponde à localização da lib libsystemc.so, a segunda não precisa mudar desde que execute esse
+#script no diretório <projeto>/processor
 export LD_LIBRARY_PATH=../../systemc-2.2.0/lib-linux:../components/lib
 ./bin/processor
