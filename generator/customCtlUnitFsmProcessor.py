0818b79bf7800af800f48926b2a757462efa879e
Julio Melo <julio.engcomp@gmail.com>
2011-07-09 11:00:38 -0300

    adicionando os geradores de código para a unidade de controle

---
+++ generator/customCtlUnitFsmProcessor.py
@@ -0,0 +1,97 @@
+import os
+import xml.dom.minidom
+from xml.dom.minidom import Node
+
+
+def findInDicList(l, key, obj): #encontra o retorna o i-esimo elemento de l cujo l[i][key] == obj
+	for i in l:
+		if i[key] == obj:
+			return i
+	return None
+
+#processa todas as fsm do diretorio
+def processFsm(fsmDir):
+	#todo process every file in the dirctory
+	files = os.listdir(fsmDir)
+	fsm_set = list()
+	for f in files:
+		print ('parsing '+ fsmDir + f)
+		doc = xml.dom.minidom.parse(fsmDir + f)
+
+		mapping = {}
+
+		transitionTable = {} #cada elemento eh uma tripla estado, proxEstado, commandos
+
+		states = list()
+		transitions = list()
+		places = list()
+		arcs = list()
+
+		initialStateId = {}
+
+		#pega todos os lugares e label atribuidas a cada um deles
+		for placeNode in doc.getElementsByTagName("place"):
+			placeId = placeNode.getAttribute("id")
+			name = placeNode.getElementsByTagName("name") #lista com tamanho 1
+			command = name[0].getElementsByTagName("value")[0].childNodes[0].nodeValue #pegando o comando primeiro filho da tag <value> filha de <name>
+			place = {'id': placeId, 'command': command}
+			initialMarkingValue = ((placeNode.getElementsByTagName("initialMarking")[0]).getElementsByTagName("value")[0]).childNodes[0].nodeValue
+			if initialMarkingValue == "1":
+				initialStateId = placeId
+			places.append(place)
+
+		#pega todas as transicoes
+		for transition in doc.getElementsByTagName("transition"):
+			transitionId = transition.getAttribute("id")
+			t = {'id':transitionId}
+			transitions.append(t)
+
+		#pega todos os arcos
+		for arcIt in doc.getElementsByTagName("arc"):
+			arcId = arcIt.getAttribute("id")
+			arcFrom = arcIt.getAttribute("source")
+			arcTo = arcIt.getAttribute("target")
+			place = findInDicList(places, "id", arcFrom)
+			arc = None
+			if place == None:
+				transition = findInDicList(transitions, "id", arcFrom)
+				place = findInDicList(places, "id", arcTo)
+				arc = {"id" : arcId, "from" : transition, "to" : place}
+			else:
+				transition = findInDicList(transitions, "id", arcTo)
+				arc = {"id" : arcId, "from" : place, "to" : transition}
+			
+			arcs.append(arc)
+
+		for t in transitions:
+			arcA = findInDicList(arcs,"to", t) #de um lugar para uma transicao
+			arcB = findInDicList(arcs,"from", t) #de uma transicao para um lugar
+			
+			actPlace = arcA["from"]
+			nextPlace = arcB["to"]
+			actState = findInDicList(states, "id", actPlace["id"])
+			if(actState == None):
+				actState = {"id" : actPlace["id"], "command" : actPlace["command"] , "nextState" : None}
+				states.append(actState)
+			nextState = findInDicList(states, "id", nextPlace["id"])
+
+			if(nextState == None):
+				nextState = {"id" : nextPlace["id"], "command" : nextPlace["command"] , "nextState" : None}
+				states.append(nextState)
+			actState["nextState"] = nextState
+		initialState = findInDicList(states, "id", initialStateId)
+		fsm = {"initialState":initialState, "states":states} #fsm para essa instrucao
+		
+		fsm_struct = {"name" : f[0:len(f)-4], "fsm" : fsm}
+
+		fsm_set.append(fsm_struct)
+
+	return fsm_set
+	#print (states)
+	#s = fsm["initialState"]
+	#s_ = s["nextState"]
+	#print (s["id"], s["command"])
+	#while not s_ == None:
+	#	s = s_
+#		print (s["id"], s["command"])
+#		s_ = s["nextState"]
---
+++ generator/customizableControllUnitGenerator.py
@@ -0,0 +1,70 @@
+import customCtlUnitFsmProcessor
+import sys
+import os
+
+#TODO gerar expandir macro
+def getMacroMicroinstructions(microInstructionMacro):
+	if microInstructionMacro == "loadsARWithPc":
+		return "ulaOp 5	ulaInAMuxSel 2 ulaOutDemuxSel 0 writeMemory 0 dRinMuxSel 1 dRoutDemuxSel 0 loadRA 1 wait 1 loadAR 1 wait 1 wait 1 loadDR 1 wait 1"
+	if microInstructionMacro == "resetAllLoads":
+		 return "rfReadWriteBit = 0 writeMemory 0 loadRA 0 loadRB 0 loadIR 0 loadAR 0 loadPC 0 loadDR 0 dRinMuxSel 1 dRoutDemuxSel 0"
+	if microInstructionMacro == "incrementPC":
+		resetAllLoads = "rfReadWriteBit = 0 writeMemory 0 loadRA 0 loadRB 0 loadIR 0 loadAR 0 loadPC 0 loadDR 0 dRinMuxSel 1 dRoutDemuxSel 0"
+		return resetAllLoads + " ulaOp 4 ulaInAMuxSel 2 ulaOutDemuxSel 2 loadRA 1 wait 1 loadPC = 1 wait 1"
+	return None
+
+	
+def isMacro(microInstructionMacro):
+ if microInstructionMacro == "incrementPC":
+	 return True
+ if microInstructionMacro == "resetAllLoads":
+	 return True
+ if microInstructionMacro == "loadsARWithPc":
+	 return True
+ return False
+
+
+#gera codigo para um conjunto de maquinas de estado dadas
+def generateCode(fsm_set):
+	codeFile = open('controlUnitCode', 'w')
+	codeFile.write("map<string,vector<string> > stateOutputMap;\n")
+	for fsmIt in fsm_set:
+		instructionName = fsmIt["name"]
+		fsm = fsmIt["fsm"]
+		codeFile.write("{ /*microinstructions for " + instructionName + "begin*/\n")
+		codeFile.write("vector<string > microInstructions;\n")
+		s = fsm["initialState"]
+		s_ = s["nextState"]
+
+		commands = ""
+		if not s == None:
+			if isMacro(s["command"]):
+				codeFile.write("/*microinstructions for macro " + s["command"] + "*/\n")
+				commands = getMacroMicroinstructions(s["command"])
+			else:
+				commands = s["command"]
+		
+		codeFile.write("microInstructions.push_back(\"" + commands + "\");\n")
+		while not s_ == None:
+			s = s_
+			if isMacro(s["command"]):
+				codeFile.write("/*microinstructions for macro " + s["command"] + "*/\n")
+				commands = getMacroMicroinstructions(s["command"])
+			else:
+				commands = s["command"]
+			codeFile.write("microInstructions.push_back(\"" + commands + "\");\n")
+			s_ = s["nextState"]
+
+		codeFile.write("stateOutputMap[\"" + instructionName + "\"] = microInstructions;\n")
+		codeFile.write("} /*microinstructions for " + instructionName + "end*/\n")
+	codeFile.close()
+
+print ("running")
+if not len(sys.argv) == 2:
+	print('usage: python customizableControllUnitGenerator.py <fsmDirectory>')
+else:
+	if os.path.isdir(sys.argv[1]):
+		fsmList = customCtlUnitFsmProcessor.processFsm(sys.argv[1])
+		generateCode(fsmList)
+	else:
+		print('usage: python customizableControllUnitGenerator.py <fsmDirectory>')
